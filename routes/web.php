f651c4bb0afb35efb991cd76776276f13bbecb74
Juubaju <julia.pereira133@etec.sp.gov.br>
2023-03-21 11:01:20 -0300

    laravel linkado

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Models\Servico;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    $primeiroRegistro = new Servico;
+    $primeiroRegistro->nome = "pintura";
+    $primeiroRegistro->preço = "R$100,00";
+    $primeiroRegistro->save();
+
+    return view('welcome');
+});
+?>
